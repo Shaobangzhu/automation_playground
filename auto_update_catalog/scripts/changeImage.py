7ddcfd47e815cc143e4734dae320f6b5522ef2a3
Chaoran Lu <andyluloveusc@gmail.com>
2024-04-07 22:57:01 -0700

    mod changeImage

--- auto_update_catalog/scripts/changeImage.py
+++ auto_update_catalog/scripts/changeImage.py
@@ -3,10 +3,10 @@
 from PIL import Image
 import os
 
-path = "./supplier-data/images/"
-for f in os.listdir("./supplier-data/images"):
+path = "../supplier_data/images/"
+for f in os.listdir("../supplier_data/images"):
     if f.endswith(".tiff"):
         split_f = f.split(".")
         name = split_f[0]+".jpeg"
         im = Image.open(path + f).convert("RGB")
-        im.resize((600,400)).save("./supplier-data/images/"+name, "JPEG")
+        im.resize((600,400)).save("../supplier_data/images/"+name, "JPEG")
